7a03699603666ae7d6454d32887311baf8ad4b6c
Abdullah <abdullahshaak@gmail.com>
2026-06-17 10:56:47 +0500

    Day 1 basics

---
+++ Day 1/script.js
@@ -0,0 +1,4 @@
+console.log("Hello, World!");
+// This is a simple JavaScript file that logs "Hello, World!" to the console.
+/* You can run this code in a web browser's console or in a Node.js environment.
+it is the first step in learning JavaScript and serves as a basic example of how to output text. it is the working of the structure i.e function of the structure */
